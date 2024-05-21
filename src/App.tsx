0d1f3700458ab765c3d488fd8da63d39693a9771
VNagorniy <vnagorniyrnd@gmail.com>
2024-05-21 13:43:05 +0300

    TypeScript. Инициализация проекта.Типизация компонента Button.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Button from './components/Button/Button';
+
+function App() {
+	return (
+		<>
+			<Button onClick={() => console.log()}>Кнопка</Button>
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,21 @@
+import { FC } from 'react';
+import './Button.css';
+import { ButtonProps } from './Button.props';
+import cn from 'classnames';
+
+export const ButtonAlt: FC<ButtonProps> = ({ children, className, ...props }) => {
+	return (
+		<button className={cn('button accent', className)} {...props}>
+			{children}
+		</button>
+	);
+};
+function Button({ children, className, ...props }: ButtonProps) {
+	return (
+		<button className={cn('button accent', className)} {...props}>
+			{children}
+		</button>
+	);
+}
+
+export default Button;
