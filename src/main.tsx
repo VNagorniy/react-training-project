72dd48291f7460f1f6bf79ab1da531a4bb06625e
VNagorniy <vnagorniyrnd@gmail.com>
2024-05-22 21:49:27 +0300

    Работа с проектом. Использование lazy функции

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import ReactDOM from 'react-dom/client';
-
 import './index.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { Menu } from './pages/Menu/Menu.tsx';
 import { Cart } from './pages/Cart/Cart.tsx';
 import { Error as ErrorPage } from './pages/Error/Error.tsx';
 import { Layout } from './layout/Layout/Layout.tsx';
 import { Product } from './pages/Product/Product.tsx';
 import axios from 'axios';
 import { PREFIX } from './helpers/API.ts';
+
+const Menu = lazy(() => import('./pages/Menu/Menu'));
 
 const router = createBrowserRouter([
 	{
