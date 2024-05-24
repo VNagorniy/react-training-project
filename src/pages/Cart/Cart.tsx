5ec92f95b63f456e2e540b1f7dbf67b0eeeb62a5
VNagorniy <vnagorniyrnd@gmail.com>
2024-05-24 14:42:49 +0300

    Работа с проектом. Подтверждение покупки

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,17 +1,24 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Heading from '../../components/Heading/Heading';
-import { RootState } from '../../store/store';
 import CartItem from '../../components/CartItem/CartItem';
 import { useEffect, useState } from 'react';
 import { Product } from '../../interfaces/product.interface';
 import axios from 'axios';
 import { PREFIX } from '../../helpers/API';
 import styles from './Cart.module.css';
+import { cartActions } from '../../store/cart.slice';
+import { useNavigate } from 'react-router-dom';
+import { AppDispatch, RootState } from '../../store/store';
+import Button from '../../components/Button/Button';
 
 const DELIVERY_FEE = 169;
 export function Cart() {
 	const [cartProducts, setCartProducts] = useState<Product[]>([]);
 	const items = useSelector((s: RootState) => s.cart.items);
+	const jwt = useSelector((s: RootState) => s.user.jwt);
+	const dispatch = useDispatch<AppDispatch>();
+	const navigate = useNavigate();
+
 	const total = items
 		.map((i) => {
 			const product = cartProducts.find((p) => p.id === i.id);
@@ -30,6 +37,22 @@ export function Cart() {
 	const loadAllItems = async () => {
 		const res = await Promise.all(items.map((i) => getItem(i.id)));
 		setCartProducts(res);
+	};
+
+	const checkout = async () => {
+		await axios.post(
+			`${PREFIX}/order`,
+			{
+				products: items
+			},
+			{
+				headers: {
+					Authorization: `Bearer ${jwt}`
+				}
+			}
+		);
+		dispatch(cartActions.clean());
+		navigate('/success');
 	};
 
 	useEffect(() => {
@@ -61,10 +84,17 @@ export function Cart() {
 			</div>
 			<hr className={styles['hr']} />
 			<div className={styles['line']}>
-				<div className={styles['text']}>Итог {items.length}</div>
+				<div className={styles['text']}>
+					Итог <span className={styles['total-count']}>({items.length})</span>
+				</div>
 				<div className={styles['price']}>
 					{total + DELIVERY_FEE}&nbsp;<span>₽</span>
 				</div>
+			</div>
+			<div className={styles['checkout']}>
+				<Button appearance="big" onClick={checkout}>
+					оформить
+				</Button>
 			</div>
 		</>
 	);
